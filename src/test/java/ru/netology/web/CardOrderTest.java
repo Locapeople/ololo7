1f7ca06117602da3a09d6b2b43f3280ee0a9e7bf
Locapeople <missis.anyack@yandex.ru>
2023-12-21 11:32:00 +0300

    fix test: selectors, SUT URI, code style

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -11,25 +11,29 @@ public class CardOrderTest {
 
     @Test
     void testCorrectInputs() {
-        open("http://0.0.0.0:9999");
-        SelenideElement form = $(".form.form_size_m.form_theme_alfa-on-white");
-        form.$("[data-test-id=name] input").setValue("Фамилия Имя");
-        form.$("[data-test-id=phone] input").setValue("+79998887766");
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button.button_view_extra.button_size_m.button_theme_alfa-on-white").click();
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Фамилия Имя");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
 
-        $("[data-test-id=order-success]").shouldBe(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldBe(
+                Condition.exactText(
+                        "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."
+                ));
     }
 
     @Test
     void testIncorrectInputs() {
-        open("http://0.0.0.0:9999");
-        SelenideElement form = $(".form.form_size_m.form_theme_alfa-on-white");
-        form.$("[data-test-id=name] input").setValue("Фамилия"); // Имя не указано, должно падать, но не падает!
-        form.$("[data-test-id=phone] input").setValue("+79998887766");
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button.button_view_extra.button_size_m.button_theme_alfa-on-white").click();
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Фамилия"); // Имя не указано, должно падать, но не падает!
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
 
-        $("[data-test-id=order-success]").shouldBe(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldBe(
+                Condition.exactText(
+                        "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."
+                ));
     }
 }
